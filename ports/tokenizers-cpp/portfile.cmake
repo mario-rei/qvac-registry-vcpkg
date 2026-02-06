3e9a84608d321bab61245aee7e725e7177ac2632
mekhakt <mekhak.shoyan@tether.io>
2026-02-06 11:54:24 -0800

    Add cpp-tokenizers  (#76)
    * Add tokenizers-cpp
    
    * minor fix
    
    * Remove port version
    
    * Change hash
    
    * Change hash
    
    * Add install targets
    
    * Revert wvac-lib-inference-addon-cpp ref
    
    * Revert qvac-lib-inference-addon-cpp version
    
    * Revert back the qvac-lib-inference-addon-cpp changes
    
    * Fix review comment
    
    * Update SHA512 for tokenizers-cpp
    
    * Fix portfile warnings
    
    ---------
    
    Co-authored-by: Juan Pablo Garibotti Arias <juan.arias@bitfinex.com>

---
+++ ports/tokenizers-cpp/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mlc-ai/tokenizers-cpp
+    REF tags/v${VERSION}
+    SHA512 e4c1a7a1f69482c4d923dbd91b1479c137dcc8f7ac8a2033f270eaf1f440d24c4f2e775a8fe4985f30cf30704de04c3102155990ce8588c76cafe4c0d33b345d
+    PATCHES
+        0001-build-only-hf-tokenizer.patch
+        0002-fix-rust-build.patch
+	0003-remove-abs-paths.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME tokenizers_cpp
+    CONFIG_PATH lib/cmake
+)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
